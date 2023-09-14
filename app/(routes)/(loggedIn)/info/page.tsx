d9ad825b03e6f7787cc166046fe1915a458b7357
Ahmed Younis <91804311+younis3@users.noreply.github.com>
2023-09-14 21:57:25 +0300

    updated info page

--- app/(routes)/(loggedIn)/info/page.tsx
+++ app/(routes)/(loggedIn)/info/page.tsx
@@ -2,7 +2,49 @@ import React from "react";
 import styles from "./info.module.scss";
 
 const page = () => {
-  return <div className={styles.comingSoon}>Coming Soon..</div>;
+  const W_VALUE = 10;
+  const LEG_W_VALUE = 15;
+  const CUP_W_VALUE = 40;
+  const MVP_VALUE = 10;
+  const ATT_VALUE = 5;
+  return (
+    <div className={styles.pageWrapper}>
+      <h2>
+        <span>EXP: </span>Total Points
+        <span className={styles.ar}>عدد النقاط الاجمالي</span>
+      </h2>
+
+      <h2>
+        <span>W: </span> Total Wins
+        <span className={styles.ar}>عدد الكلي للانتصارات في المباريات</span>
+        <span className={styles.scoreVal}>+{W_VALUE}EXP</span>
+      </h2>
+
+      <h2>
+        <span>LegW: </span>Total League Wins
+        <span className={styles.ar}>فوز في معرخوت</span>
+        <span className={styles.scoreVal}>+{LEG_W_VALUE}EXP</span>
+      </h2>
+
+      <h2>
+        <span>CupW: </span>Zachi Cup Wins
+        <span className={styles.ar}>فوز في كأس الزاتشي</span>
+        <span className={styles.scoreVal}>+{CUP_W_VALUE}EXP</span>
+      </h2>
+
+      <h2>
+        <span>MVP: </span>Total MVP Titles
+        <span className={styles.ar}>عدد القاب افضل لاعب</span>
+        <span className={styles.scoreVal}>+{MVP_VALUE}EXP</span>
+      </h2>
+
+      <h2>
+        <span>Att: </span> Total Appearances{" "}
+        <span className={styles.ar}>حضور</span>
+        <span className={styles.scoreVal}>+{ATT_VALUE}EXP</span>
+      </h2>
+    </div>
+  );
 };
 
 export default page;
