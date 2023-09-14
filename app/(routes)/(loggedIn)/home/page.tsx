34a6b1b3e4cdd8be7660ca0f9b1c32e776aa0300
Ahmed Younis <91804311+younis3@users.noreply.github.com>
2023-09-14 20:05:14 +0300

    small changes

--- app/(routes)/(loggedIn)/home/page.tsx
+++ app/(routes)/(loggedIn)/home/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import NavMobile from "@/app/_components/nav/mobile-nav/NavMobile";
-import Nav from "../../../_components/nav/horizontal/Nav";
 import ScoreTable from "../../../_components/score-table/ScoreTable";
 import styles from "./home.module.scss";
 import Image from "next/image";
